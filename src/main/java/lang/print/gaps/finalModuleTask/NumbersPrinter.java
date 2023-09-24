3614f4fec9169ee39e4a20ab542119a4702faf8d
githubirakli <118377638+githubirakli@users.noreply.github.com>
2023-09-24 23:57:37 +0400

    Update NumbersPrinter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -1,7 +1,12 @@
 package lang.print.gaps.finalModuleTask;
-
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-
-    }
+        boolean result=(number>0);
+            System.out.println(result);
+ }
+    public static void main(String[] args){
+        NumbersPrinter printer= new NumbersPrinter();
+        int number=10;
+        printer.printIsPositive(number);
+}
 }

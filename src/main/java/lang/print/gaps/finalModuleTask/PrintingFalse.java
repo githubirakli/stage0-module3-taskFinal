3465a4147d9cca9a35bcad8151c9ad47a37531b9
githubirakli <118377638+githubirakli@users.noreply.github.com>
2023-09-24 23:40:17 +0400

    Update PrintingFalse.java

--- src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
+++ src/main/java/lang/print/gaps/finalModuleTask/PrintingFalse.java
@@ -1,9 +1,9 @@
 package lang.print.gaps.finalModuleTask;
-
 public class PrintingFalse {
     public static void main(String[] args) {
         int first = 9;
         int second = -17;
-        //write code here
+       boolean result=(first<second);
+           System.out.println(result);
     }
 }

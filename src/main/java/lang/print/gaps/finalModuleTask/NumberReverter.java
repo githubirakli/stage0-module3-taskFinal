27fe2b45e6bbeea206b4860ff0731a0070a2e1e0
githubirakli <118377638+githubirakli@users.noreply.github.com>
2023-09-25 00:48:43 +0400

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,24 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        if (number >= 100 && number <= 999) { // Check if it's a 3-digit number
+            int digit1 = number % 10;        // Extract the units digit
+            int digit2 = (number / 10) % 10; // Extract the tens digit
+            int digit3 = number / 100;       // Extract the hundreds digit
+
+            // Revert the number by rearranging the digits
+            int revertedNumber = digit1 * 100 + digit2 * 10 + digit3;
+            
+            // Print the reverted number
+            System.out.println("Reverted number: " + revertedNumber);
+        } else {
+            System.out.println("Input is not a 3-digit number");
+        }
+    }
+
+    public static void main(String[] args) {
+        NumberReverter reverter = new NumberReverter();
+        int numberToRevert = 489;
+        reverter.revert(numberToRevert); // Should print "Reverted number: 984"
     }
 }

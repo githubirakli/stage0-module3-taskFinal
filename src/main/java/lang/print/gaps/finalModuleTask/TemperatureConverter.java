ccaf79d79ec2fa902a673d970d2c33a5ae078823
githubirakli <118377638+githubirakli@users.noreply.github.com>
2023-09-25 00:25:47 +0400

    Update TemperatureConverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        float fahrenheit =temperatureCelsius*9/5+32;
+        System.out.println(fahrenheit);
     }
+    public static void main(String[] args){
+        TemperatureConverter converter= new TemperatureConverter();
+        int temperatureCelsius=32;
+        converter.toFahrenheit(temperatureCelsius);
+} 
 }

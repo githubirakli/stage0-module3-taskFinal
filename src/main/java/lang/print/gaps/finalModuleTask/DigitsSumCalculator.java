2b1139b329428823f5825fc3d3a8b0cf6a1616c2
githubirakli <118377638+githubirakli@users.noreply.github.com>
2023-09-25 01:15:13 +0400

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,19 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        if(number>=1000&&number<=9999){
+            int digit1=number%10;
+             int  digit2=(number%100-digit1)/10;
+              int digit3=(number%1000-number%100)/100;
+               int digit4=(number/1000);
+               System.out.println(digit1+digit2+digit3+digit4);
+        }else{
+            System.out.println("Input is not a four-digit number");
+        }
+        }
+        public static void main(String[] args) {
+            DigitsSumCalculator sumOfDigits= new DigitsSumCalculator();
+            int number= 8888;
+            sumOfDigits.calculateSum(number);
     }
 }
